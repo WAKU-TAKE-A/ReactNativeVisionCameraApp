2cc9bf34db23646a1259d5b80f6b13abcb4bdf74
WAKU-TAKE-A <waku-take-a@ymail.ne.jp>
2025-01-02 15:59:27 +0900

    とりあえず動いた。でも、ビルド時にWarningが残っている。
    注意点
    * react-native-permissionsとreact-native-reanimatedを主導で追加
    * AndroidManifest.xmlのカメラ権限追加

--- App.tsx
+++ App.tsx
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { StyleSheet, View, Button } from 'react-native';
+import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera';
 
 export default function App() {
+  const [isCameraActive, setIsCameraActive] = useState(false);
+  const device = useCameraDevice('back')
+  const { hasPermission } = useCameraPermission()
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    // カメラの権限をリクエスト
+    (async () => {
+      const cameraPermission = await Camera.requestCameraPermission();
+      
+      // CameraPermissionRequestResult を確認
+      if (!hasPermission) {
+        console.log('カメラの権限が許可されていません');
+      }
+    })();
+  }, []);
+
+  if (device == null) return null; // デバイスがロードされるまで待機
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {isCameraActive && (
+        <Camera
+          style={StyleSheet.absoluteFill}
+          device={device}
+          isActive={true}
+          ref={cameraRef}
+          photo={true} // 静止画キャプチャを有効
+          video={false}
+        />
+      )}
+      <View style={styles.buttonContainer}>
+        <Button
+          title={isCameraActive ? 'ストップ' : 'スタート'}
+          onPress={() => setIsCameraActive(!isCameraActive)}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +47,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'black',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 50,
+    alignSelf: 'center',
   },
 });
